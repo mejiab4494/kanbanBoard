a115a6590eab5a98b267d82fc5c3882e4259e547
Bryan Mejia <mejiab2@gmail.com>
2026-04-13 23:39:43 -0400

    prepare the kanban board in html added a few lines in css

---
+++ script.js
@@ -0,0 +1,21 @@
+const todo = document.querySelector('.toDo');
+const inProgress = document.querySelector('.inProgress');
+const done = document.querySelector('.done');
+const taskBtn = document.querySelector(".addTaskButton")
+
+
+function addTask() {
+    console.log("click")
+}
+
+
+
+
+
+
+
+
+
+
+
+taskBtn.addEventListener("click", addTask)
